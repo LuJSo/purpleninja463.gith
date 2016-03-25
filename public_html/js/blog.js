8a1865bdf9fd1f3c65767ef50d4e6aeaf306975a
purpleninja463 <sushiguy132@gmail.com>
2016-03-25 11:03:56 -0700

    finished video 11

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,14 +1,20 @@
 $(function () {
-    var APPLICATION_ID = "C2363073-8C3B-0C93-FF26-967C1BEF1800"
-    SECRET_KEY = "FB43A048-05CB-F78E-FF04-19001DA98100"
+    var APPLICATION_ID = "C2363073-8C3B-0C93-FF26-967C1BEF1800",
+    SECRET_KEY = "FB43A048-05CB-F78E-FF04-19001DA98100",
     VERSION = "v1";
     
     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
     
-    var user = new Backendless.User();
-    user.email = "sushiguy132@gmail.com";
-    user.password ="lucas463";
-    Backendless.UserService.register(user);
+    var dataStore = Backendless.Persistence.of(Posts);
+    var post = new Posts({title: "My First Blog Post", content:"My first blog post content", email:"email@email.com"});
+    dataStore.save(post);
 });
+
+function Posts(args){
+    args = args || {};
+    this.title= args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.emailEmail || "";
+}
 
 
